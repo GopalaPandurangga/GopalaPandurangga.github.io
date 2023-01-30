0513fd50e42433e4f5f13fa1a04d9463ebffe92d
palzy <test@MacBook-Pro.local>
2023-01-30 14:31:47 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,51 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  darkMode: 'class',
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+
+    extend: {
+      animation: {
+        blob: "blob 7s infinite",
+      },
+      keyframes: {
+        blob: {
+          "0%": {
+            transform: "translate(0px, 0px) scale(1)",
+          },
+          "33%": {
+            transform: "translate(30px, -50px) scale(1.1)",
+          },
+          "66%": {
+            transform: "translate(-20px, 20px) scale(0.9)",
+          },
+          "100%": {
+            transform: "tranlate(0px, 0px) scale(1)",
+          },
+        },
+      },
+      spacing: {
+        "big": "48rem"
+      },
+
+      colors: {
+        primary: '#f97316',
+        secondary: "#F4F2ED",
+        dark: "#252525"
+      },
+
+
+      screens: {
+        "sm": "480px",
+        '2xl': '1320px',
+      },
+
+
+    },
+  },
+  plugins: [],
+}
